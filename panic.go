d3649310f8d221e5b25824f6ae44de709cf44bc7
Adi Wahyudi <adi.whyd02@gmail.com>
2025-08-19 20:07:29 +0700

    learn: recover

--- panic.go
+++ panic.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Panic
 // The panic function is used to stop the program immediately.
@@ -8,15 +10,24 @@ import "fmt"
 // Once panic is triggered, the program will terminate,
 // but any defer functions will still be executed before the program fully stops.
 
+// Recover
+// The recover function is used to catch data from a panic.
+// By using recover, the panic process is stopped, allowing the program to continue running.
+
 func endApp() {
 	fmt.Println("End App")
+
+	message := recover()
+	if message != nil {
+		fmt.Println("Error message: ", message)
+	}
 }
 
 func runApp(error bool) {
 	defer endApp()
 
 	if error {
-		panic("ERROR")
+		panic("Unexpected error occurred")
 	}
 
 	fmt.Println("Processing")
@@ -24,4 +35,6 @@ func runApp(error bool) {
 
 func main() {
 	runApp(true)
+
+	fmt.Println("Program still continue...")
 }
