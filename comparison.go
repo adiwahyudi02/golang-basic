644fb8f3213420f4e3f518783b976ffb9c2c1a65
Adi Wahyudi <adi.whyd02@gmail.com>
2025-08-19 15:20:44 +0700

    learn: comparison

---
+++ comparison.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// comparison operator
+	// > is greater than
+	// < is less than
+	// >= is greater than or equal to
+	// <= is less than or equal to
+	// == is equal to
+	// != is not equal to
+
+	fmt.Println("1 > 2 = ", 1 > 2)
+	fmt.Println("1 < 2 = ", 1 < 2)
+	fmt.Println("1 >= 2 = ", 1 >= 2)
+	fmt.Println("1 <= 2 = ", 1 <= 2)
+	fmt.Println("1 == 2 = ", 1 == 2)
+	fmt.Println("1 != 2 = ", 1 != 2)
+	fmt.Println("adi == adi = ", "adi" == "adi")
+	fmt.Println("adi != wahyudi = ", "adi" != "wahyudi")
+}
