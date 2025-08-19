8ba54d22b9ec9719c41c3364e2f20c711c795678
Adi Wahyudi <adi.whyd02@gmail.com>
2025-08-19 14:42:48 +0700

    learn: string type

---
+++ string.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Adi")
+	fmt.Println("Wahyudi")
+
+	// Function for string type
+	// len("string") => length of string
+	// "string"[index] => character at index, it will return byte
+	fmt.Println(len("Adi"))
+	fmt.Println("Adi"[0])
+	fmt.Println("Adi"[1])
+	fmt.Println("Adi"[2])
+}
