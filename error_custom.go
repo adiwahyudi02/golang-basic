05cd36a394d556c3724ea7c5f0fe9ae48f8014dc
Adi Wahyudi <adi.whyd02@gmail.com>
2025-08-20 12:32:06 +0700

    learn: custom error

---
+++ error_custom.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+/*
+	Creating a Custom Error
+
+	Since error is an interface, if we want to create our own error,
+	we can define a struct that follows the contract of the error interface.
+*/
+
+type validationError struct {
+	Message string
+}
+
+func (v *validationError) Error() string {
+	return v.Message
+}
+
+type notFoundError struct {
+	Message string
+}
+
+func (n *notFoundError) Error() string {
+	return n.Message
+}
+
+func SaveData(id string, data any) error {
+	if id == "" {
+		return &validationError{"validation error"}
+	}
+
+	if id != "Dadang" {
+		return &notFoundError{"data not found"}
+	}
+
+	// ok
+	return nil
+}
+
+func main() {
+	err := SaveData("Dadang", nil)
+	if err != nil {
+		//if validationErr, ok := err.(*validationError); ok {
+		//	fmt.Println("validation error:", validationErr.Error())
+		//} else if notFoundErr, ok := err.(*notFoundError); ok {
+		//	fmt.Println("not found error:", notFoundErr.Error())
+		//} else {
+		//	fmt.Println("unknown error:", err.Error())
+		//}
+
+		switch finalError := err.(type) {
+		case *validationError:
+			fmt.Println("validation error:", finalError.Error())
+		case *notFoundError:
+			fmt.Println("not found error:", finalError.Error())
+		default:
+			fmt.Println("unknown error:", finalError.Error())
+		}
+
+	} else {
+		fmt.Println("Success")
+	}
+}
