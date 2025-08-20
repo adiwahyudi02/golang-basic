573fb6484e2184937088ae4894fb7e65b06f8c13
Adi Wahyudi <adi.whyd02@gmail.com>
2025-08-20 11:35:19 +0700

    learn: package and import

---
+++ helper/helper.go
@@ -0,0 +1,13 @@
+package helper
+
+/*
+	Package
+
+	A package is a way to organize the code in a Go program.
+	By using packages, we can structure and tidy up our code.
+	Essentially, a package corresponds to a folder (directory) in the operating system.
+*/
+
+func SayHello(name string) string {
+	return "Hello " + name
+}
