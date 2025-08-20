1bc58b895d110ed7df3244f33ac72849b1e7a4ba
Adi Wahyudi <adi.whyd02@gmail.com>
2025-08-20 11:42:18 +0700

    learn: access modifier

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,7 @@
 package helper
 
+import "fmt"
+
 /*
 	Package
 
@@ -7,6 +9,29 @@ package helper
 	By using packages, we can structure and tidy up our code.
 	Essentially, a package corresponds to a folder (directory) in the operating system.
 */
+
+/*
+	Access Modifier
+
+	In other programming languages, there are keywords to define access modifiers
+	for functions or variables.
+	In Go, access modifiers are determined simply by the case of the name:
+	- If the name starts with an uppercase letter, it can be accessed from other packages.
+	- If the name starts with a lowercase letter, it cannot be accessed from other packages.
+*/
+
+var version = "1.0.0"
+var Application = "golang-basic"
+
+func sayGoodBye(name string) string {
+	return "Good bye " + name
+}
+
+func Example() {
+	fmt.Println(Application)
+	fmt.Println(version)
+	fmt.Println(sayGoodBye("Adi"))
+}
 
 func SayHello(name string) string {
 	return "Hello " + name
--- import.go
+++ import.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	result := helper.SayHello("Adi")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) // undefined: helper.version
+	// fmt.Println(helper.sayGoodBye("Adi")) // undefined: helper.sayGoodBye
 }
